c9d4899a3c32667f396b3a3f45aaf6e0ccfc4e1b
Юлия Юдина <yudina.lia31@yandex.ru>
2023-08-21 11:11:29 +0400

    second commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,45 +1,48 @@
 const form = document.getElementById('signin__form');
-const inputFields = Array.from(document.querySelectorAll('.control'));
-const userId = document.getElementById('user_id');
-const signin = document.querySelector('.signin');
-const welcome = document.querySelector('.welcome');
-const button = document.querySelector('.btn');
-let savedId;
+const welcome = document.getElementById('welcome');
+const signin = document.getElementById('signin');
+const logoutBtn = document.getElementById('logout__btn');
 
-form.addEventListener('submit', (event) => {
-    fetch('https://students.netoservices.ru/nestjs-backend/auth', {
-        method: 'POST',
-        body: new FormData(form)
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Неверный логин/пароль');
-            }
-        })
-        .then(data => {
-            inputFields.forEach(field => {
-                field.value = '';
-            });
-            localStorage.setItem('id', data.user_id);
-            savedId = localStorage.getItem('id');
-            userId.textContent = savedId;
-            signin.classList.remove('signin_active');
-            welcome.classList.add('welcome_active');
-        })
-        .catch(error => {
-            alert(error);
-        });
+// Событие изменения в форме авторизации и отправка запроса на сервер
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    event.preventDefault();
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        if(xhr.response.success) {
+            const userId = xhr.response.user_id;
+            localStorage.setItem('userId', userId);
+            logoutBtn.classList.add('logout__btn_active');
+            welcomeDude(userId);
+        } else {
+            alert('Неверный логин или пароль');
+        }
+    }        
+    xhr.send(formData);
+    form.reset();
 });
 
-window.addEventListener('load', () => {
-    savedId = localStorage.getItem('id');
-    if (savedId) {
-        userId.textContent = savedId;
-        signin.classList.remove('signin_active');
-        welcome.classList.add('welcome_active');
+window.addEventListener('DOMContentLoaded', () => {
+    const userId = localStorage.getItem('userId');
+    if(userId) {
+        welcomeDude(userId);
     }
 });
+
+// Событие клика на кнопку Выйти
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem('userId');
+    signin.classList.add('signin_active');
+    welcome.classList.remove('welcome_active');
+    logoutBtn.classList.remove('logout__btn_active')    
+})
+
+// Функция приветствия пользователя 
+function welcomeDude(userId) {
+    signin.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+    welcome.querySelector('#user_id').textContent = userId;
+}
